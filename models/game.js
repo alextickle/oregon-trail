05ddbb58166cf8708e4b0cbc5faaeac52882c225
Alex Tickle <alexander.tickle@gmail.com>
2017-06-03 22:15:37 -0700

    added favicon

--- models/game.js
+++ models/game.js
@@ -24,7 +24,7 @@ module.exports = function(sequelize, DataTypes) {
         });
       },
 
-      var compare = function(a, b){
+      compare: function(a, b){
         if (a.name[0] < b.name[0]){
           return -1;
         }
